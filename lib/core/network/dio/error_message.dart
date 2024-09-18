2d162ee53cc9d00cac90306d1e265ed5d853811c
Dam Hong Duc <hongduc001h@gmail.com>
2024-09-18 10:28:49 +0700

    feat: rm un-use code

--- lib/core/network/dio/error_message.dart
+++
@@ -1,7 +0,0 @@
-import 'package:easy_localization/easy_localization.dart';
-
-class DioErrorMessage {
-  static String getErrorMessage(String key) {
-    return "backEndError.$key".tr();
-  }
-}
--- lib/core/resources/app_colors.dart
+++ lib/core/resources/app_colors.dart
@@ -1,31 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  //light
   static const Color primary_light = Color(0xff009EF7);
-  static const Color text_light = Color(0xFF000000);
-
-  static const Color transparent = Color(0x00000000);
-  static const Color success = Color(0xFF5cb85c);
-  static const Color error = Color(0xFFff0033);
-  static const Color info = Color(0xFF02afcd);
-  static const Color warning = Color(0xFFffab00);
 
   static const Color black_text = Color(0xFF000000);
-  static const Color white_text = Color(0xFFffffff);
-
-  static const Color black = Color(0xFF000000);
   static const Color white = Color(0xFFffffff);
-  static const Color red = Color(0xFFFF0000);
-  static const Color grey_light = Color(0xFFA3A5A8);
-  static const Color gray95 = Color(0xFF89CFF0);
-
-  static const Color secondaryButton = Color(0xff008DDA);
-  static const Color primary_background = Color(0xFFf6f8fa);
-  static const Color secondary_background = Color(0xFFffffff);
-
-  static const Color grey_text = Color(0xffa1a5b7);
-  static const Color red_text = Color(0xfff0506e);
 
   static const Color successToast = Color(0xFF48aa54);
   static const Color errorToast = Color(0xFFe7443c);
